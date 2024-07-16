f773400390ded8e770937d166b0d44c295db1ffa
qwerty <qwerty@gmail.com>
2024-07-16 15:45:44 +0500

    first

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './assets/style/main.scss'
+import VueLazyload from 'vue-lazyload'
+import App from './App.vue'
+
+// Делаем компонент глобальным
+
+// Подключаем иконки
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas,far,fab)
+
+const app = createApp(App)
+const pinia = createPinia()
+
+
+// import errorimage from '@/assets/img/nophoto.png'
+app.use(VueLazyload, {
+    error: errorimage,
+})
+
+app.use(routers)
+app.use(pinia)
+app.component('fa', FontAwesomeIcon)
+app.mount('#app')
+
+
+
+
